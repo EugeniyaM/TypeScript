85f68dfc7d2436b45cb4a81eb101c25c559f48b6
Jane <volegzhaninaem@list.ru>
2021-03-28 21:51:45 +0700

    absctract classes commit

--- 5_classes.ts
+++ 5_classes.ts
@@ -45,3 +45,16 @@ class Cat extends Animal {
 const cat = new Cat()
 
 //==============================
+abstract class Component {
+    abstract render(): void
+    abstract info(): string
+}
+
+class AppComponent extends Component {
+    render(): void {
+        console.log('Component on render')
+    }
+    info(): string {
+        return 'This is info';
+    }
+}
