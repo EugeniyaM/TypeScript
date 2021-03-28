0256de2cda69a73e37ac5390cc9a10bd3292dfcd
Jane <volegzhaninaem@list.ru>
2021-03-28 15:38:00 +0700

    Functions commit

--- 1types.js
+++ 1types.js
@@ -1,2 +1,27 @@
-var str = 'Hello';
-console.log(str);
+var isFetching = true;
+var isLoading = false;
+var int = 42;
+var float = 4.2;
+var num = 3e10;
+var message = 'Hello Typescript';
+var numberArray = [1, 1, 2, 3, 5, 8, 13];
+var numberArray2 = [1, 1, 2, 3, 5, 8, 13];
+var words = ['Hello', 'TS'];
+//Tuple
+var contact = ['Jane', 123456];
+//Any
+var variable = 42;
+variable = 'New String';
+//====================
+function sayMyName(name) {
+    console.log(name);
+}
+sayMyName('Хайзенберг');
+// Never - 1) Функция возвращает ошибку 2)Постоянно что-то делает
+function throwError(message) {
+    throw new Error(message);
+}
+function infinite() {
+    while (true) {
+    }
+}
--- 1types.ts
+++ 1types.ts
@@ -1,2 +1,41 @@
-const str: string = 'Hello';
- console.log(str);
+const isFetching: boolean = true;
+const isLoading: boolean = false;
+
+const int: number = 42;
+const float: number = 4.2;
+const num: number = 3e10;
+
+const message: string = 'Hello Typescript'
+
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13]
+const numberArray2: Array<number> = [1, 1 , 2 , 3, 5, 8, 13]
+
+const words: string[] = ['Hello', 'TS']
+
+//Tuple
+const contact: [string, number] = ['Jane', 123456]
+
+//Any
+let variable: any = 42
+variable = 'New String'
+
+//====================
+function sayMyName(name: string): void { //void - функция ничего не возвращает
+ console.log(name)
+}
+
+sayMyName('Хайзенберг')
+
+// Never - 1) Функция возвращает ошибку 2)Постоянно что-то делает
+
+function  throwError(message: string):never {
+ throw new Error(message)
+}
+
+function infinite():never {
+ while (true) {
+
+ }
+}
+
+
