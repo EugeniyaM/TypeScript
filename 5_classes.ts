16670a5d7265f775c7ceef4f4325774244827d59
Jane <volegzhaninaem@list.ru>
2021-03-28 21:39:50 +0700

    Modifications commit

--- 5_classes.ts
+++ 5_classes.ts
@@ -25,3 +25,23 @@ class Car {
     }
 }
 
+// ==================
+
+class Animal {
+    protected voice: string = ''
+    public color: string = 'black'
+
+    private go() {
+        console.log('Go')
+    }
+}
+
+class Cat extends Animal {
+    public setVoice(voice: string): void {
+        this.voice = voice
+    }
+}
+
+const cat = new Cat()
+
+//==============================
